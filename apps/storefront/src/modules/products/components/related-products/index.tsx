abfc3bb17d88d6442093e6c76dff088bf5ce1902
Bradi Manensi Mayorquín Mejía <bradimayorquin1994@gmail.com>
2026-09-05 01:24:56 +0000

    Quitar titulo duplicado en productos relacionados

--- apps/storefront/src/modules/products/components/related-products/index.tsx
+++ apps/storefront/src/modules/products/components/related-products/index.tsx
@@ -49,9 +49,6 @@ export default async function RelatedProducts({
   return (
     <div className="product-page-constraint">
       <div className="flex flex-col items-center text-center mb-16">
-        <span className="text-base-regular text-gray-600 mb-6">
-          Productos relacionados
-        </span>
         <p className="text-2xl-regular text-ui-fg-base max-w-lg">
           Puede que también te interesen estos productos.
         </p>
