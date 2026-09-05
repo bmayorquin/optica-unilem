f3341f8f0a3869ac472daa94ebcd13c2f3da1c0c
Bradi Manensi Mayorquín Mejía <bradimayorquin1994@gmail.com>
2026-09-05 20:28:29 +0000

    Ocultar colecciones vacias y ajustar grid de productos en home

--- apps/storefront/src/modules/home/components/featured-products/product-rail/index.tsx
+++ apps/storefront/src/modules/home/components/featured-products/product-rail/index.tsx
@@ -21,7 +21,7 @@ export default async function ProductRail({
     },
   })
 
-  if (!pricedProducts) {
+  if (!pricedProducts || pricedProducts.length === 0) {
     return null
   }
 
@@ -57,7 +57,7 @@ export default async function ProductRail({
           Ver todo
         </InteractiveLink>
       </div>
-      <ul className="grid grid-cols-2 small:grid-cols-3 gap-x-6 gap-y-24 small:gap-y-36">
+      <ul className="grid grid-cols-2 small:grid-cols-3 medium:grid-cols-4 gap-x-6 gap-y-10 small:gap-y-16">
         {pricedProducts &&
           pricedProducts.map((product) => (
             <li key={product.id}>
