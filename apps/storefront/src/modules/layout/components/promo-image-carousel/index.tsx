3a62bfeaac806ed623a4f7094faadfac2ed84dc7
Bradi Manensi Mayorquín Mejía <bradimayorquin1994@gmail.com>
2026-09-06 21:20:05 +0000

    Ajustar carrusel de promo a formato 24:9

--- apps/storefront/src/modules/layout/components/promo-image-carousel/index.tsx
+++ apps/storefront/src/modules/layout/components/promo-image-carousel/index.tsx
@@ -32,7 +32,7 @@ const PromoImageCarousel = ({ images }: PromoImageCarouselProps) => {
   }
 
   return (
-    <div className="group overflow-hidden aspect-[26/9] w-full relative bg-unilen-cream">
+    <div className="group overflow-hidden aspect-[24/9] w-full relative bg-unilen-cream">
       {images.map((url, index) => (
         <img
           key={url}
